019e3ed0ab5eda41f240b2b8bfd3d1fc8d3cde0d
Justin2172021 <justin.busching@gmail.com>
2022-04-18 16:27:09 -0500

    Added functions for the input form

--- objectConstructors.js
+++ objectConstructors.js
@@ -16,6 +16,7 @@ function Book(Title, Author, Pages, Read) {
 function addBookToLibrary(Title, Author, Pages, Read) {
     let book = new Book(Title, Author, Pages, Read);
     myLibrary.push(book);
+    displayBooksOnPage();
 }
 
 // const theHobbit = new Book("The Hobbit", " by J.R.R. Tolkien", "295 Pages", "not Read yet");
@@ -24,6 +25,13 @@ function addBookToLibrary(Title, Author, Pages, Read) {
 // Function to display library array to cards
 function displayBooksOnPage() {
     const books = document.querySelector(".books");
+
+    // Remove all previously displayed cards before I loop over array again
+    const removeDivs = document.querySelectorAll(".card");
+    console.log("show me the node count of current card divs....", removeDivs);
+    for (let i = 0; i < removeDivs.length; i++) {
+        removeDivs[i].remove();
+    }
     
     // Loop over the library array and display to the cards
     myLibrary.forEach(myLibrary => {
@@ -54,15 +62,53 @@ function displayBooksOnPage() {
     // }
 }
 
+// Start event listener/display form to add a new book to library
+const addBookButton = document.querySelector(".add-book-button")
+addBookButton.addEventListener("click", displayTheForm);
+
+function displayTheForm() {
+    document.getElementById("add-book-form").style.display = "";
+}
+
+// Start event listener/add input to array for new entry form
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", intakeFormData);
+
+// Transform form data to variables for intake
+function intakeFormData() {
+    let Title = document.getElementById("Title").value;
+    let Author = document.getElementById("Author").value;
+    let Pages = document.getElementById("Pages").value;
+    let Read = document.getElementById("Read").value;
+    
+    // Break out if form is incomplete or not valid
+    if ((Title == "") || (Author == "") || (Pages == "") || (Read == "")) {
+        return;
+    }
+    // Call function to input the book data to array
+    addBookToLibrary(Title, Author, Pages, Read);
+
+    // Reset the form after successful submission
+    document.getElementById("add-book").reset();
+}
+
+// Start event listener for clear form button
+const clearButton = document.querySelector(".reset-button");
+clearButton.addEventListener("click", clearForm);
+
+function clearForm() {
+    document.getElementById("add-book").reset();
+}
+
 // Calling function and adding data manually for now until the form is built out
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 Pages", "Not Read yet");
-addBookToLibrary("The Seven Habits of Highly Effective People", "Steven Covey", "200 Pages", "Read");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 Pages", "Not Read yet");
-addBookToLibrary("The Seven Habits of Highly Effective People", "Steven Covey", "200 Pages", "Read");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 Pages", "Not Read yet");
-addBookToLibrary("The Seven Habits of Highly Effective People", "Steven Covey", "200 Pages", "Read");
-addBookToLibrary("The Seven Habits of Highly Effective People", "Steven Covey", "200 Pages", "Read");
+// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 Pages", "Not Read yet");
+// addBookToLibrary("The Seven Habits of Highly Effective People", "Steven Covey", "200 Pages", "Read");
+// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 Pages", "Not Read yet");
+// addBookToLibrary("The Seven Habits of Highly Effective People", "Steven Covey", "200 Pages", "Read");
+// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 Pages", "Not Read yet");
+// addBookToLibrary("The Seven Habits of Highly Effective People", "Steven Covey", "200 Pages", "Read");
+// addBookToLibrary("The Seven Habits of Highly Effective People", "Steven Covey", "200 Pages", "Read");
 
-console.log("End of code array contents", myLibrary);
+// console.log("End of code array contents", myLibrary);
 
-displayBooksOnPage();
+// displayBooksOnPage();
